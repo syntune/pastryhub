dcf2643dc22fd5edc502373c92e75598b9fafa8a
horizon <89583473+syntune@users.noreply.github.com>
2025-04-26 16:50:57 +0700

    Update 77_ZWTLM.lua

--- 77_ZWTLM.lua
+++ 77_ZWTLM.lua
@@ -1,5 +1,5 @@
-coroutine.wrap(function()
-    game.Loaded:Wait()
+    if not game:IsLoaded() then game.Loaded:Wait() end
+
     print("Game is loaded.")
     game.ReplicatedStorage.B2K:FindFirstChild("4107679d-3b41-4c87-96dd-7fbe6566aa36"):FireServer("E_RANK_HUNTER", 0)
     wait(2)
@@ -7,4 +7,3 @@ coroutine.wrap(function()
     wait(1)
     game.ReplicatedStorage.B2K:FindFirstChild("f4cd9a60-367a-4407-be6b-bb1ac28900cc"):FireServer()
     queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/syntune/pastryhub/refs/heads/main/77_ZWTLM.lua'))()")
-end)()
